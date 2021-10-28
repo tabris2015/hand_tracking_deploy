be5f8a5742f1d1bcfbedcc1586ec5ca37e90f04b
Jose Laruta <eduardo.laruta@gmail.com>
2021-10-28 01:03:06 -0400

    removed styles

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import time
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 
 
 def dist_3d(punto1, punto2):
@@ -112,9 +111,7 @@ def main():
                     mp_drawing.draw_landmarks(
                         frame,
                         hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS,
-                        mp_drawing_styles.get_default_hand_landmarks_style(),
-                        mp_drawing_styles.get_default_hand_connections_style()
+                        mp_hands.HAND_CONNECTIONS
                     )
             else:
                 print("no se detectan manos!")
